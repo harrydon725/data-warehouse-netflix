d49831447a13f6ec20196a920f5221a3004e8f5f
Harry Donnelly <harrydonnelly@MASTUDENT-Harry-Donnelly-N2YXJXDGHM.local>
2025-11-05 14:45:09 +0000

    prewarp commit

---
+++ dbt_projects/target/run/maker_warehouse/models/facts/genres.sql
@@ -0,0 +1,19 @@
+
+  
+    
+
+  create  table "mydb"."dbt_warehouse"."genres__dbt_tmp"
+  
+  
+    as
+  
+  (
+    
+
+
+SELECT
+    DISTINCT
+    genres
+FROM mydb.public.raw_netflix
+  );
+  
---
+++ dbt_projects/target/run/maker_warehouse/models/facts/test.sql
@@ -0,0 +1,17 @@
+
+  create view "mydb"."dbt_warehouse"."test__dbt_tmp"
+    
+    
+  as (
+    
+
+select
+
+    
+    'bank_transfer'
+    
+    'credit_card'
+    
+    'gift_card'
+    
+  );
